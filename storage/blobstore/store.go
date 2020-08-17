fdef727a68822ce1176d3821cd67a5809ffd65db
Yannic Rieger <yannic.rieger@hetzner-cloud.de>
2020-08-17 17:03:34 +0200

    Fill function bodies

--- storage/blobstore/store.go
+++ storage/blobstore/store.go
@@ -9,19 +9,31 @@ type Store struct {
 }
 
 // GetMap retrieves a map identified by the given digest from the underlying storage.
-func (store *Store) GetMap(digest string) ([]byte, error) {}
+func (store *Store) GetMap(digest string) ([]byte, error) {
+	return nil, nil
+}
 
 // GetMap retrieves a configuration identified by the given digest from the underlying storage.
-func (store *Store) GetConfig(digest string) (string, error) {}
+func (store *Store) GetConfig(digest string) (string, error) {
+	return "", nil
+}
 
 // GetTag retrieves a tag for a specific map inside a repository form the underlying storage.
-func (store *Store) GetTag(repository, mapName, tagName string) (Tag, error) {}
+func (store *Store) GetTag(repository, mapName, tagName string) (Tag, error) {
+	return Tag{}, nil
+}
 
 // PutMap saves a map to the underlying storage making it identifiable by its content.
-func (store *Store) PutMap(mapblob []byte) error {}
+func (store *Store) PutMap(mapblob []byte) error {
+	return nil
+}
 
 // PutConfig saves a configuration to the underlying storage making it identifiable by its content.
-func (store *Store) PutConfig(content string) error {}
+func (store *Store) PutConfig(content string) error {
+	return nil
+}
 
 // PutTag saves a tag for a map inside a repository to the underlying storage.
-func (store *Store) PutTag(repository, mapName, tagName string, tag Tag) error {}
+func (store *Store) PutTag(repository, mapName, tagName string, tag Tag) error {
+	return nil
+}
--- storage/metastore/driver/filesystem.go
+++ storage/metastore/driver/filesystem.go
@@ -5,9 +5,9 @@ type filesystemDriver struct {
 }
 
 func (fd *filesystemDriver) Get(repository, mapName, key string) (interface{}, error) {
-
+	return nil, nil
 }
 
 func (fd *filesystemDriver) Set(repository, mapName, key string, value interface{}) error {
-
+	return nil
 }
