8bb929ebb68d5592d0169b12f631c60adc66bb06
Yannic Rieger <riegeryannic@gmail.com>
2020-08-29 20:02:23 +0200

    Read metadata from disk

--- storage/metastore/driver/driver.go
+++ storage/metastore/driver/driver.go
@@ -3,5 +3,5 @@ package driver
 type Driver interface {
 	Get(repository, mapName, key string) (interface{}, error)
 
- 	Set(repository, mapName, key string, value interface{}) error
+	Set(repository, mapName, key string, value interface{}) error
 }
--- storage/metastore/driver/filesystem.go
+++ storage/metastore/driver/filesystem.go
@@ -1,10 +1,56 @@
 package driver
 
-type filesystemDriver struct {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
+var (
+	ErrKeyNotFound = "key not found"
+)
+
+type filesystemDriver struct {
+	basePath string
 }
 
 func (fd *filesystemDriver) Get(repository, mapName, key string) (interface{}, error) {
+	path := fmt.Sprintf("%s/%s/%s", fd.basePath, repository, mapName)
+
+	f, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	r := bufio.NewReader(f)
+
+	for err != io.EOF {
+		data, err := r.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		// lines have the following format:
+		// key=value\n
+		// key=value\n
+
+		parts := strings.Split(data, "=")
+		if len(parts) < 2 {
+			continue
+		}
+
+		if parts[0] == key {
+			return parts[1], nil
+		}
+	}
+
+	// TODO:
+	// * loop through lines
+	// * if startsWith(key)
+	// * split("=") -> return parts[1]
+
 	return nil, nil
 }
 
